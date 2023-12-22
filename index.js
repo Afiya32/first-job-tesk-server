92f58d4ae3f033034574dab42de10b698387bda9
Afiya32 <biplobguru123@gmail.com>
2023-12-22 07:54:54 +0600

    server completed

--- index.js
+++ index.js
@@ -26,12 +26,50 @@ async function run() {
   //  database collection
   const tesksCollection = client.db('TeskManagementDB').collection('Tesks');
 
-// pages
+// read data
 app.get("/tesks", async (req, res) => {
   const curser = tesksCollection.find();
   const result = await curser.toArray();
   res.send(result);
+});
+// add data
+
+
+app.post('/tesks',async(req,res)=>{
+  const newTesk = req.body;
+  console.log(newTesk);
+  const result = await tesksCollection.insertOne(newTesk);
+  res.send(result);
+});
+// deleted data
+
+app.delete('/tesks/:id', async (req, res)=>{
+  const id = req.params.id;
+  const query= {_id: new ObjectId(id)}
+  const result= await tesksCollection.deleteOne(query);
+  res.send(result);
 })
+// single tesks
+app.get('/tesks/:id', async (req, res) => {
+  const id = req.params.id;
+console.log('ID:', id);
+const query = { _id: new ObjectId(id) };
+console.log('Query:', query);
+  try {
+    const result = await tesksCollection.findOne(query);
+    if (result) {
+      res.send(result);
+    } else {
+      res.status(404).send('tesk is not found');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+  
+  
+});
+
 
 
 
